cf347d89e491f91e06152ae05aab22bed0d41a5c
Michael Lorton <mlorton@yahoo.com>
2025-05-31 09:24:12 -0700

    updated the UI a tad

--- src/components/layout/public-layout.tsx
+++ src/components/layout/public-layout.tsx
@@ -1,19 +1,11 @@
-import { ReactNode } from 'react';
-import { Header } from './header';
-import { Footer } from './footer';
+import { FC, PropsWithChildren } from "react";
+import { Footer } from "./footer";
+import { Header } from "./header";
 
-interface PublicLayoutProps {
-  children: ReactNode;
-}
-
-export function PublicLayout({ children }: PublicLayoutProps) {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1 pt-16">
-        {children}
-      </main>
-      <Footer />
-    </div>
-  );
-}
+export const PublicLayout: FC<PropsWithChildren> = ({ children }) => (
+  <div className="flex flex-col min-h-screen bg-[url('/bg-paper.png')] bg-repeat bg-auto">
+    <Header />
+    <main className="flex-1 pt-16">{children}</main>
+    <Footer />
+  </div>
+);
