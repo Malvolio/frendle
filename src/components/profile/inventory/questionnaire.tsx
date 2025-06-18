fcf542d9b9da896597e1ad03456385005f7c343d
Michael Lorton <mlorton@yahoo.com>
2025-06-18 13:00:15 -0700

    improved questionaire ui

--- src/components/profile/inventory/questionnaire.tsx
+++ src/components/profile/inventory/questionnaire.tsx
@@ -1,8 +1,8 @@
 import Spinner from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
 import useProfileInterests from "@/hooks/useProfileInterests";
+import { cn } from "@/lib/utils";
 import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
-import { motion } from "motion/react";
 import { FC, useEffect, useState } from "react";
 import { RoughNotation } from "react-rough-notation";
 import { Questions } from "./Questions";
@@ -28,56 +28,49 @@ const DisplayQuestion: FC<{
     <h2 className="text-3xl mb-6 font-bold text-[#373737]">{text}</h2>
     <form className="w-full">
       <RadioGroup onValueChange={setSelectedId} value={selectedId}>
-        <motion.div
-          className="flex flex-row gap-4 w-[100%]  h-72"
-          initial={{ x: 20, opacity: 0.5 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ duration: 0.3 }}
-        >
+        <div className="flex flex-row gap-4 w-full  h-72">
           {options.map((option) => {
             const isSelected = selectedId === option.id;
             return (
-              <RoughNotation
+              <RadioGroupItem
                 key={option.id}
-                type="box"
-                show={isSelected}
-                iterations={8}
-                color={isSelected ? "#000000" : "#8F8F8F"}
-                animate={false}
+                value={option.id.toString()}
+                id={option.id.toString()}
               >
-                <label
-                  className="flex flex-col h-full justify-around items-center gap-2 py-2 max-w-72 cursor-pointer border border-black/30 px-2 w-full"
-                  htmlFor={option.id}
+                <RoughNotation
+                  type="box"
+                  show={isSelected}
+                  iterations={5}
+                  color={isSelected ? "#000000" : "#8F8F8F"}
+                  animate={false}
                 >
-                  <div className="text-2xl h-[80%]">
+                  <label
+                    className="flex flex-col h-full justify-around items-center gap-1 py-1 max-w-72 cursor-pointer border border-black/30 px-2 w-full"
+                    htmlFor={option.id}
+                  >
                     <img
                       src={`/inventory/q${id}-opt${option.id}.png`}
-                      // width={"140px"}
-                      className={
-                        "m-auto transition-all max-h-36 w-fit" +
-                        (isSelected ? "scale-110 " : "scale-100 ")
-                      }
+                      className={cn("m-auto transition-all max-h-36 w-fit", {
+                        "scale-110 ": isSelected,
+                        "scale-100 ": !isSelected,
+                      })}
                     />
-                  </div>
-                  <div>
-                    <RadioGroupItem
-                      value={option.id.toString()}
-                      id={option.id.toString()}
-                    />
-                    <span
-                      className={
-                        "px-2 text-2xl  leading-tight block mt-0 " +
-                        (isSelected ? "font-semibold" : "")
-                      }
-                    >
-                      {option.text}
-                    </span>
-                  </div>
-                </label>
-              </RoughNotation>
+                    <div>
+                      <span
+                        className={
+                          "px-2 text-2xl  leading-tight block mt-0 " +
+                          (isSelected ? "font-semibold" : "")
+                        }
+                      >
+                        {option.text}
+                      </span>
+                    </div>
+                  </label>
+                </RoughNotation>
+              </RadioGroupItem>
             );
           })}
-        </motion.div>
+        </div>
       </RadioGroup>
     </form>
   </>
