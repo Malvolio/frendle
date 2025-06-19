d637eeb1e1ba04b54db5cbe8665be3fd6ddd0dbf
Michael Lorton <mlorton@yahoo.com>
2025-06-19 12:38:23 -0700

    Try the Numb TURN server

--- src/lib/useWebRTC.ts
+++ src/lib/useWebRTC.ts
@@ -30,9 +30,9 @@ const DEFAULT_CONFIG: RTCConfiguration = {
     // { urls: "stun:stun.l.google.com:19302" },
     // { urls: "stun:stun1.l.google.com:19302" },
     {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject",
+      urls: ["turn:numb.viagenie.ca"],
+      credential: "muazkh",
+      username: "webrtc@live.com",
     },
   ],
   iceTransportPolicy: "relay",
