150c34556266329bf3852eb141d3acc4e5f530ac
Michael Lorton <mlorton@yahoo.com>
2025-06-19 12:19:59 -0700

    removed TURN service

--- src/lib/useWebRTC.ts
+++ src/lib/useWebRTC.ts
@@ -27,8 +27,8 @@ export type WebRTCHookReturn = {
 // Default Configuration
 const DEFAULT_CONFIG: WebRTCConfig = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
+    // { urls: "stun:stun.l.google.com:19302" },
+    // { urls: "stun:stun1.l.google.com:19302" },
     {
       urls: "turn:openrelay.metered.ca:80",
       username: "openrelayproject",
