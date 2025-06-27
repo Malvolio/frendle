35d790e887ee60cd81573167fff3df2075d46b8f
Michael Lorton <mlorton@yahoo.com>
2025-06-27 13:10:24 -0700

    hack to hide self-meetings

--- ui/src/components/home/PairUps.tsx
+++ ui/src/components/home/PairUps.tsx
@@ -1,5 +1,6 @@
 import { EnrichedSession } from "@/hooks/useGetSessions";
-import { X } from "lucide-react";
+import { useAuth } from "@/providers/auth-provider";
+import { ArrowRight, X } from "lucide-react";
 import React from "react";
 import PauseButton from "./PauseButton";
 
@@ -30,58 +31,66 @@ const formatDate = (date: Date) => {
 const PairUps: React.FC<{
   sessions: EnrichedSession[];
   isPaused: boolean;
-}> = ({ sessions, isPaused }) => (
-  <>
-    <p className="my-2 px-6">Here’s your upcoming matches:</p>
+}> = ({ sessions, isPaused }) => {
+  const { user } = useAuth();
+  return (
+    <>
+      <p className="my-2 px-6">Here’s your upcoming matches:</p>
 
-    <div className="space-y-4  w-80">
-      {sessions.map((pairUp, index) => {
-        const dateInfo = formatDate(new Date(pairUp.scheduled_for));
+      <div className="space-y-4  w-80">
+        {sessions.map((pairUp, index) => {
+          const dateInfo = formatDate(new Date(pairUp.scheduled_for));
 
-        return (
-          <div
-            key={index}
-            className="group flex items-center justify-between py-3 border-b last:border-b-0 hover:bg-[#58B4AE] px-6 gap-2 transition-all"
-          >
-            <div className="flex items-center space-x-4">
-              {/* Date card */}
-              <div className="bg-white border-2 border-[#4C4B4B] rounded-sm transform -rotate-6 border-r-4 border-b-4 border-t-8 ">
-                <div className="text-center">
-                  <div className="text-xl font-bold text-gray-900 px-2">
-                    {dateInfo.day}
+          return (
+            <div
+              key={index}
+              className="group flex items-center justify-between py-3 border-b last:border-b-0 hover:bg-[#58B4AE] px-6 gap-2 transition-all"
+            >
+              <div className="flex items-center space-x-4">
+                {/* Date card */}
+                <div className="bg-white border-2 border-[#4C4B4B] rounded-sm transform -rotate-6 border-r-4 border-b-4 border-t-8 ">
+                  <div className="text-center">
+                    <div className="text-xl font-bold text-gray-900 px-2">
+                      {dateInfo.day}
+                    </div>
+                    <div className="text-xs pb-2 ">
+                      {dateInfo.month} {dateInfo.date}
+                    </div>
                   </div>
-                  <div className="text-xs pb-2 ">
-                    {dateInfo.month} {dateInfo.date}
+                </div>
+
+                {/* Content */}
+                <div>
+                  <div className="font-bold text-gray-900 group-hover:text-white">
+                    {pairUp.partner_profile.name === user?.public_profile.name
+                      ? "Charlotte" // silly hack!
+                      : pairUp.partner_profile.name}
+                  </div>
+                  <div className="text-gray-600 text-sm max-w-52 group-hover:text-white">
+                    {pairUp.partner_profile.bio}
                   </div>
                 </div>
               </div>
 
-              {/* Content */}
-              <div >
-                <div className="font-bold text-gray-900 group-hover:text-white">
-                  {pairUp.partner_profile.name}
-                </div>
-                <div className="text-gray-600 text-sm max-w-52 group-hover:text-white">
-                  {pairUp.partner_profile.bio}
-                </div>
-              </div>
+              <button className="group border border-black/40 w-8 h-8 rounded-full  hover:bg-[#BA4F76]  flex items-center justify-center transition-colors">
+                {pairUp.partner_profile.name === user?.public_profile.name ? (
+                  <X className="w-4 h-4 text-gray-600 group-hover:text-white" />
+                ) : (
+                  <ArrowRight />
+                )}
+              </button>
             </div>
-
-            {/* Close button */}
-            <button className="group border border-black/40 w-8 h-8 rounded-full  hover:bg-[#BA4F76]  flex items-center justify-center transition-colors">
-              <X className="w-4 h-4 text-gray-600 group-hover:text-white" />
-            </button>
+          );
+        })}
+        {isPaused && (
+          <div className="flex text-center text-gray-500 gap-2">
+            <div className="text-left">Pair-ups are currently paused. </div>
+            <PauseButton />
           </div>
-        );
-      })}
-      {isPaused && (
-        <div className="flex text-center text-gray-500 gap-2">
-          <div className="text-left">Pair-ups are currently paused. </div>
-          <PauseButton />
-        </div>
-      )}
-    </div>
-  </>
-);
+        )}
+      </div>
+    </>
+  );
+};
 
 export default PairUps;
